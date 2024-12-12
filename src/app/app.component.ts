7739b07377a58deed71313c8bd86c66ae5b64c30
Albin David C <albindavidc007@gmail.com>
2024-12-12 09:40:52 +0530

    Completed created first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'first_angular_app';
-}
+
+export class AppComponent {}
