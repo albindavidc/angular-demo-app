5adf70dbfcf32f78aa4917725f3e1dfc2bdcec59
Albin David C <albindavidc007@gmail.com>
2024-12-12 13:38:57 +0530

    tried signals * computed methods

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -7,8 +7,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
-export class UserComponent { 
-  selectedUser = DUMMY_USERS[randomIndex]
+export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+
+  // get imagePath(){
+  //   return 'assets/users/' + this.selectedUser.avatar
+  // }
+
+  onSelectUser(){
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
+  }
 }
