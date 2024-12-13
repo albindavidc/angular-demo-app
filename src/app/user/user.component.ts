a62b2a6069d7ac6145b63162526c68f58b668b9d
Albin David C <albindavidc007@gmail.com>
2024-12-13 12:33:29 +0530

    created task and new-task component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input} from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +8,14 @@ import { Component, Input} from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required:true}) avatar!:string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) user!: User;
+  @Input({required:true}) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
 
-  get imagePath(){
-    return 'assets/users/' + this.avatar;
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
   }
-  onSelectUser(){
+  onSelectUser() {
+    this.select.emit(this.user.id);
   }
 }
