94107360ab2985e4aec34deee55db9c4550d7a8e
Albin David C <albindavidc007@gmail.com>
2024-12-28 08:16:38 +0530

    add production build for github pages

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+}).catch((err) => console.error(err));
+
+
